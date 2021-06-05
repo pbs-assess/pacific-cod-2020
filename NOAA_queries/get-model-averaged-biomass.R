0df63024f4c275edefb6794bcb06aa2929d169c6
robynforrest <robynforrest@gmail.com>
2021-06-04 18:25:04 -0700

    updating biomass

--- NOAA_queries/get-model-averaged-biomass.R
+++ NOAA_queries/get-model-averaged-biomass.R
@@ -1,10 +1,13 @@
 # Get survey and biomass time series
+# For Steve Barbeaux (NOAA)
+# Robyn Forrest. June 4 2020
+message("STOP! Have you run the chunks in report/00-load.Rmd?")
 
 # !! 1. Run all chunks in report/00-load.Rmd !!
 # This gets the objects avg.model.3cd and avg.model.5abcd
 
 # Now run this code
-
+library(here)
 source(here("NOAA_queries","get-model-averaged-biomass-func.R"))
 
 # Plot the biomass
@@ -25,9 +28,27 @@ p <- b.plot(avg.model.5abcd,
 ggsave(here("NOAA_queries","figs","Model_averaged_biomass_pcod_5ABCD.png"))
 
 # Just get the numbers
-b <- b.plot(avg.model.3cd,
-            base.model.3cd.name,
+b3cd <- b.csv(avg.model.3cd,
+            "Model average",
             depl = FALSE,
-            add.hist.ref = FALSE,
             french=french)
-glimpse(b)
+glimpse(b3cd)
+
+b5abcd <- b.csv(avg.model.5abcd,
+              "Model average",
+              depl = FALSE,
+              french=french)
+glimpse(b5abcd)
+
+# Check
+p <- b3cd %>%
+  ggplot() +
+  geom_ribbon(aes(x=Year, ymin=lowercv, ymax=uppercv), color="lightgray", alpha=0.5)+
+  geom_line(aes(x=Year, y=median), lwd=2)
+p
+
+p <- b5abcd %>%
+  ggplot() +
+  geom_ribbon(aes(x=Year, ymin=lowercv, ymax=uppercv), color="lightgray", alpha=0.5)+
+  geom_line(aes(x=Year, y=median), lwd=2)
+p
